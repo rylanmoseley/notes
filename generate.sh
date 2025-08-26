d2c07b0281b7bcc27efed26f0471b524822fd265
rylan moseley <rylanmoseley@gmail.com>
2025-08-25 20:30:50 -0400

    add error handling to temp generator

--- generate.sh
+++ generate.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Usage: $0 <directory>"
+  exit 1
+fi
+
+if [ ! -d "$1" ]; then
+  echo "Error: Directory $1 does not exist."
+  exit 1
+fi
+
+if [ ! -f "$1/000-metadata.md" ]; then
+  echo "Error: 000-metadata.md not found in $1."
+  exit 1
+fi
+
 mkdir temp
 cd "$1"
 cp * ../temp
 cd ../temp
 echo "---" >> 000-metadata.md
-pandoc -f markdown-implicit_figures -o temp.pdf --toc *.md
+shopt -s extglob
+pandoc -o temp.pdf --toc !(README).md
 mv temp.pdf ../
 cd ..
 rm -r temp
